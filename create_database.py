070efe942b7c202c3e6da0de9b03b455bef3d369
msaounera9610 <msaounera@aubay.com>
2024-06-25 07:54:24 +0000

    apply RAG to calista functions

--- create_database.py
+++ create_database.py
@@ -1,28 +1,26 @@
-# from langchain.document_loaders import DirectoryLoader
-from langchain_community.document_loaders import DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
-# from langchain.embeddings import OpenAIEmbeddings
-from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
-import openai 
-from dotenv import load_dotenv
+from langchain_community.embeddings.huggingface import HuggingFaceInferenceAPIEmbeddings
 import os
 import shutil
+import re
 
-# Load environment variables. Assumes that project contains .env file with API keys
-load_dotenv()
-#---- Set OpenAI API key 
-# Change environment variable name from "OPENAI_API_KEY" to the name given in 
-# your .env file.
-openai.api_key = os.environ['OPENAI_API_KEY']
 
 CHROMA_PATH = "chroma"
-DATA_PATH = "data/books"
+#DATA_PATH = "data/books"
 
 
-def main():
-    generate_data_store()
+def extract_docstrings(code: str) -> list:
+    docstring_pattern = re.compile(r'""".*?"""', re.DOTALL)
+    return docstring_pattern.findall(code)
+
+def extract_function_names(code: str) -> list:
+    function_pattern = re.compile(r'def\s+(?P<func_name>\w+)\s*\(.*?\)\s*->\s*.*?:', re.DOTALL)
+    
+    matches = function_pattern.finditer(code)
+    function_names = [match.group('func_name') for match in matches]
+
+    return function_names
 
 
 def generate_data_store():
@@ -32,20 +30,18 @@ def generate_data_store():
 
 
 def load_documents():
-    loader = DirectoryLoader(DATA_PATH, glob="*.md")
-    documents = loader.load()
-    return documents
+    with open('functions_calista.txt', 'r+') as file:
+        doc = file.read()
+        return doc
 
 
-def split_text(documents: list[Document]):
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=300,
-        chunk_overlap=100,
-        length_function=len,
-        add_start_index=True,
-    )
-    chunks = text_splitter.split_documents(documents)
-    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+def split_text(document):
+    chunks = []
+    docstrings = extract_docstrings(document)
+    funcs = extract_function_names(document)
+    for i, docstring in enumerate(docstrings):
+        metadata = {"start_index": funcs[i]}
+        chunks.append(Document(page_content=docstring, metadata=metadata))
 
     document = chunks[10]
     print(document.page_content)
@@ -59,13 +55,19 @@ def save_to_chroma(chunks: list[Document]):
     if os.path.exists(CHROMA_PATH):
         shutil.rmtree(CHROMA_PATH)
 
+    #model_id = 'sentence-transformers/all-MiniLM-L6-v2'
+    model_id = 'mixedbread-ai/mxbai-embed-large-v1'
+    model = HuggingFaceInferenceAPIEmbeddings(api_key='',
+                                                  api_url=f"https://api-inference.huggingface.co/pipeline/feature-extraction/{model_id}"
+                                                  )
+
     # Create a new DB from the documents.
     db = Chroma.from_documents(
-        chunks, OpenAIEmbeddings(), persist_directory=CHROMA_PATH
+        chunks, model, persist_directory=CHROMA_PATH
     )
     db.persist()
     print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
 
 
 if __name__ == "__main__":
-    main()
+    generate_data_store()
--- query_data.py
+++ query_data.py
@@ -1,52 +1,22 @@
-import argparse
-# from dataclasses import dataclass
 from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
-from langchain_openai import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
+from langchain_community.embeddings.huggingface import HuggingFaceInferenceAPIEmbeddings
+
 
 CHROMA_PATH = "chroma"
 
-PROMPT_TEMPLATE = """
-Answer the question based only on the following context:
+#query_text = 'i want to check if my colum is not null and that is an iban'
+#query_text = 'je veux verifier que ma colonne est un numéro de téléphone'
+query_text = 'validate phone number'
+#model_id = 'sentence-transformers/all-MiniLM-L6-v2'
+model_id = 'mixedbread-ai/mxbai-embed-large-v1'
+embedding_function = HuggingFaceInferenceAPIEmbeddings(api_key='',
+                                                api_url=f"https://api-inference.huggingface.co/pipeline/feature-extraction/{model_id}"
+                                                )
+query_text = f'Represent this sentence for searching relevant passages: {query_text}'
+db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
-{context}
-
----
-
-Answer the question based on the above context: {question}
-"""
-
-
-def main():
-    # Create CLI.
-    parser = argparse.ArgumentParser()
-    parser.add_argument("query_text", type=str, help="The query text.")
-    args = parser.parse_args()
-    query_text = args.query_text
-
-    # Prepare the DB.
-    embedding_function = OpenAIEmbeddings()
-    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
-
-    # Search the DB.
-    results = db.similarity_search_with_relevance_scores(query_text, k=3)
-    if len(results) == 0 or results[0][1] < 0.7:
-        print(f"Unable to find matching results.")
-        return
-
-    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
-    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-    prompt = prompt_template.format(context=context_text, question=query_text)
-    print(prompt)
-
-    model = ChatOpenAI()
-    response_text = model.predict(prompt)
-
-    sources = [doc.metadata.get("source", None) for doc, _score in results]
-    formatted_response = f"Response: {response_text}\nSources: {sources}"
-    print(formatted_response)
-
-
-if __name__ == "__main__":
-    main()
+# Search the DB.
+results = db.similarity_search_with_relevance_scores(query_text, k=3)
+#print(results)
+for i, result in enumerate(results):
+    print(f"result {i}:\n{result}\n")
